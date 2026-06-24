1f506d2da5eaa415d65726cafc6614bd7a56752f
Dev Patel <devpatel1828king@gmail.com>
2026-06-24 14:17:13 +0530

    updated client url

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.disable("x-powered-by");
 app.use(
   cors({
     
-    origin: [process.env.CLIENT_URL || "http://localhost:5173"],
+    origin: [process.env.CLIENT_URL || "https://codevector-frontend-zeta.vercel.app/"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
